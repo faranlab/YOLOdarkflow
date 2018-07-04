93af836c1ace68010400abbdf963860b9ee039a7
Faran <faran954@gmail.com>
2018-07-03 23:17:30 -0400

    Add files via upload

---
+++ createdataset.py
@@ -0,0 +1,73 @@
+import os
+import uuid
+import cv2
+
+ANOPATH='/home/fm/Desktop/Dataset/Video_Annotation/'
+VIDPATH='/home/fm/Desktop/Dataset/Videos/'
+IMDIR = '/home/fm/Desktop/darkflow-master/newmodeldata/images/'
+ANOSAVEDIR = '/home/fm/Desktop/darkflow-master/newmodeldata/annotations/'
+dirs=os.listdir(ANOPATH)
+
+def main():
+    for filename in dirs:
+        farzaframes={}
+        label='uav'
+        if '.txt' not in filename:
+            continue
+
+        with open(foldername + filename) as file:
+            lines=file.readlines()
+
+        key = 1
+        for line in lines:
+            line = line.strip()
+            uavcount=line.count('(')
+            detections=line.split('(')
+            farzaframes[key]=[];
+            if uavcount == 0:
+                continue;
+            for i in range(1,len(detections)):
+                xy=detections[i].split(', ')
+                xy=xy[:4]
+                xy[3]=xy[3].strip('),')
+                for j in range(len(xy)):
+                    xy[j]=int(xy[j])
+                [y1, x1, y2, x2]=xy
+                farzaframes[key].append(tuple([label, x1, y1, x2, y2]))
+            key = key + 1
+        write_xml(farzaframes)
+def write_xml(farzaframes):
+    if not os.path.isdir(ANOSAVEDIR):
+        os.mkdir(ANOSAVEDIR)
+
+    uavcnt = len(farzaframes[key])
+    data_point_name = str(uuid.uuid4())
+    image = cv2.imread(img.path)
+    height, width, depth = image.shape
+
+    annotation = ET.Element('annotation')
+    ET.SubElement(annotation, 'folder').text = IMDIR
+    ET.SubElement(annotation, 'filename').text = data_point_name + '.jpg'
+    ET.SubElement(annotation, 'segmented').text = '0'
+    size = ET.SubElement(annotation, 'size')
+    ET.SubElement(size, 'width').text = str(width)
+    ET.SubElement(size, 'height').text = str(height)
+    ET.SubElement(size, 'depth').text = str(depth)
+    for x in range(0,uavcnt)
+        ob = ET.SubElement(annotation, 'object')
+        ET.SubElement(ob, 'name').text = 'uav'
+        ET.SubElement(ob, 'pose').text = 'Unspecified'
+        ET.SubElement(ob, 'truncated').text = '0'
+        ET.SubElement(ob, 'difficult').text = '0'
+        bbox = ET.SubElement(ob, 'bndbox')
+        ET.SubElement(bbox, 'xmin').text = str(farzaframes[key][x][1])
+        ET.SubElement(bbox, 'ymin').text = str(farzaframes[key][x][2])
+        ET.SubElement(bbox, 'xmax').text = str(farzaframes[key][x][3])
+        ET.SubElement(bbox, 'ymax').text = str(farzaframes[key][x][4])
+
+    xml_str = ET.tostring(annotation)
+    root = etree.fromstring(xml_str)
+    xml_str = etree.tostring(root, pretty_print=True)
+    save_path = os.path.join(ANOSAVEDIR, img.name.replace('jpg', 'xml'))
+    with open(save_path, 'wb') as temp_xml:
+        temp_xml.write(xml_str)
